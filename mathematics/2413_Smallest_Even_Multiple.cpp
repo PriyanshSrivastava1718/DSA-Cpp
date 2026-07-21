900ab89e31e5cf5ada1a9d2c65dee2035700a9e4
Priyansh Srivastava <priyanshsrivastava70@gmail.com>
2026-07-21 19:43:53 +0530

    add Leetcode 2413: Smallest Even Multiple

---
+++ mathematics/2413_Smallest_Even_Multiple.cpp
@@ -0,0 +1,18 @@
+/* 
+class Solution {
+public:
+    int smallestEvenMultiple(int n) 
+    {
+        if(n%2==0)
+            return n;
+        else 
+            return n*2;
+    }
+};
+*/
+/* 
+algorithm - if n = even then it is already divisible by 2 
+            if n is od then odd * 2 is always even 
+            time complexity = O(1)
+            space complexity = O(1)
+ */
