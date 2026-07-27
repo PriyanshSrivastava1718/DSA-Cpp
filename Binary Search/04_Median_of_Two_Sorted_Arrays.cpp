4b23196f5a17d55a162126e115f6a2380aaad16b
Priyansh Srivastava <priyanshsrivastava70@gmail.com>
2026-07-27 12:51:17 +0530

    leetcode 04: Median of Two Sorted Arrays

---
+++ Binary Search/04_Median_of_Two_Sorted_Arrays.cpp
@@ -0,0 +1,92 @@
+/* class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
+    {
+        if(nums1.size() > nums2.size())
+        {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+
+        int s = 0;
+        int e = n1;
+
+        while(s <= e)
+        {
+            int cut1 = (s + e) / 2;
+            int cut2 = (n1 + n2 + 1) / 2 - cut1;
+
+            int left1, left2, right1, right2;
+
+            if(cut1 == 0)
+                left1 = INT_MIN;
+            else
+                left1 = nums1[cut1 - 1];
+
+            if(cut2 == 0)
+                left2 = INT_MIN;
+            else
+                left2 = nums2[cut2 - 1];
+
+            if(cut1 == n1)
+                right1 = INT_MAX;
+            else
+                right1 = nums1[cut1];
+
+            if(cut2 == n2)
+                right2 = INT_MAX;
+            else
+                right2 = nums2[cut2];
+
+            if(left1 <= right2 && left2 <= right1)
+            {
+                if((n1 + n2) % 2 == 0)
+                {
+                    int a = max(left1, left2);
+                    int b = min(right1, right2);
+
+                    return (a + b) / 2.0;
+                }
+                else
+                {
+                    return max(left1, left2);
+                }
+            }
+            else if(left1 > right2)
+            {
+                e = cut1 - 1;
+            }
+            else
+            {
+                s = cut1 + 1;
+            }
+        }
+
+        return 0;
+    }
+}; */
+/*
+Algorithm: Binary Search
+
+Approach:
+
+- Always perform Binary Search on the smaller array.
+
+- Divide both arrays into two halves using cut1 and cut2.
+
+- Check whether the partition is correct by comparing
+  the left and right elements of both arrays.
+
+- If the partition is correct:
+    - Return the middle element for odd length.
+    - Return the average of the two middle elements for even length.
+
+- Otherwise, move the Binary Search left or right
+  depending on the comparison.
+
+Time Complexity: O(log(min(m,n)))
+
+Space Complexity: O(1)
+*/
